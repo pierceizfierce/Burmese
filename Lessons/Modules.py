0d1689bfc6dea9520d56beee0d8e874f0155f8bc
Damori.Pierce <pierceizfierce@yahoo.com>
2023-10-23 18:27:04 -0400

    Commiting Modules lesson

--- Lessons/Modules.py
+++ Lessons/Modules.py
@@ -2,10 +2,9 @@
 # Moduels are files with python code in them
 # used to orgnize code into multiple files (reference converters.py)
 # Modules should contain all the related functions and classes
-import utils
-from utils import find_max
-import converters
-from converters import kg_to_lbs
+from Lessons.utils import find_max
+from Lessons import converters, utils
+from Lessons.converters import kg_to_lbs
 # #ctrl+space after the "import" will show all the functions in the module
 # #Above is the way to import specific parts of a module instead of the whole thing:
 print(kg_to_lbs(101)) #this is for the specific function from the module called. you don't have to prefix the module
