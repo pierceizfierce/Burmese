9a237ca9038cfe7b9d72ab20d0a7bb732b06fd25
Damori.Pierce <pierceizfierce@yahoo.com>
2023-11-01 00:15:59 -0400

    Added notes

--- Network_Automation/Netmiko_usage/netmiko_usage_v4.py
+++ Network_Automation/Netmiko_usage/netmiko_usage_v4.py
@@ -1,6 +1,7 @@
 import os
 from netmiko import ConnectHandler
 
+# define switches to configure
 iosv0_l2 = {
     'device_type': 'cisco_ios',
     'ip': '10.0.0.100',
@@ -41,6 +42,7 @@ iosv5_l2 = {
     'password': 'pierce1',  # DO NOT HARDCODE PASSWORDS
 }
 
+# define files to read
 sw_config = os.path.dirname(os.path.abspath(__file__))
 filename = os.path.join(sw_config, r'C:\Users\dpierce\PycharmProjects\Burmese\Network_Automation\source_lists\iosv_l2_cisco_access_switch_design.txt')
 
@@ -48,10 +50,10 @@ with open(filename) as f:
     lines = f.read().splitlines()
 print(lines)
 
+# define targets
 all_devices = [iosv0_l2, iosv1_l2, iosv2_l2, iosv3_l2, iosv4_l2, iosv5_l2]
 
 for devices in all_devices:
     net_connect = ConnectHandler(**devices)
     output = net_connect.send_config_set(lines)
-    # output = net_connect.send_command('show ip int brief')
     print(output)
