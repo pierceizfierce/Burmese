6ac3cc7ccd58435b6682fd4e7a506d946fb9a23a
pierceizfierce <pierceizfierce@yahoo.com>
2024-01-21 20:15:44 -0600

    Cleanup code

--- Server Updates and Patching/MultiServerUpdateandPatching.py
+++ Server Updates and Patching/MultiServerUpdateandPatching.py
@@ -1,6 +1,6 @@
 import paramiko
 import winrm
-
+import pandas as pd
 
 # Assuming your Excel file has columns 'IP', 'Hostname', 'OS', 'Username', and 'Password'
 df = pd.read_excel('servers.xlsx')
@@ -13,7 +13,21 @@ for index, row in df.iterrows():
 
     # Perform actions based on the OS type (Linux or Windows)
     if os_type.lower() == 'linux':
-    # Use Paramiko for Linux servers
+            # Use Paramiko for Linux servers
     # ...
     elif os_type.lower() == 'windows':
-# Use pywinrm for Windows servers
+    # Use pywinrm for Windows servers
+
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect('linux_server_ip', username='your_username', password='your_password') # remove username and password
+
+stdin, stdout, stderr = ssh.exec_command('sudo apt update && sudo apt upgrade -y')
+print(stdout.read().decode())
+ssh.close()
+
+session = winrm.Session('windows_server_ip', auth=('your_username', 'your_password'))
+encoded_ps = f'[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12; Invoke-Expression (New-Object System.Net.WebClient).DownloadString(\'https://chocolatey.org/install.ps1\'); choco upgrade all -y'
+
+result = session.run_ps(encoded_ps)
+print(result.std_out.decode())
--- Server Updates and Patching/ServerUpdatesandPatching.py
+++ Server Updates and Patching/ServerUpdatesandPatching.py
@@ -4,14 +4,16 @@ import pandas as pd
 
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect('linux_server_ip', username='your_username', password='your_password')
+ssh.connect('linux_server_ip', username='your_username', password='your_password')  # remove username and password
 
 stdin, stdout, stderr = ssh.exec_command('sudo apt update && sudo apt upgrade -y')
 print(stdout.read().decode())
 ssh.close()
 
 session = winrm.Session('windows_server_ip', auth=('your_username', 'your_password'))
-encoded_ps = f'[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12; Invoke-Expression (New-Object System.Net.WebClient).DownloadString(\'https://chocolatey.org/install.ps1\'); choco upgrade all -y'
+encoded_ps = f'[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12; ' \
+             f'Invoke-Expression (New-Object System.Net.WebClient).DownloadString(' \
+             f'\'https://chocolatey.org/install.ps1\'); choco upgrade all -y '
 
 result = session.run_ps(encoded_ps)
 print(result.std_out.decode())
