ea24fbdd376e39c5be5ce70fc05a6915433cf395
pierceizfierce <69944364+pierceizfierce@users.noreply.github.com>
2024-01-21 19:08:56 -0600

    Ansible playbook (#1)
    * Cleanup code
    
    * Cleanup code
    
    * Cleanup code

--- Lessons/AgeCalculator.py
+++ Lessons/AgeCalculator.py
@@ -10,9 +10,9 @@ weight_in_kg = int(weight) / 2.205
 print("You weigh", int(weight_in_kg), "kg")
 
 
-#''' - triple quotes are used to do multi-line strings, like email messages
+# ''' - triple quotes are used to do multi-line strings, like email messages
 
-## if you want to get a specific character of a string use [].
+# # if you want to get a specific character of a string use [].
 # print(xyz[2]) #this will get the second character starting from 0
 # print(xyz[-1]) #this will start counting from the end of the string backwards
 # print(xyz[4:10}) #This will skip the first 3 characters and end at the 10th
