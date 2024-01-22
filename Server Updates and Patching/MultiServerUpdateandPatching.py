fb6eaab8ed768419d1299b3b6741a94a2f7c0c6e
pierceizfierce <pierceizfierce@yahoo.com>
2024-01-22 01:23:13 -0600

    Fixed indention issue

--- Server Updates and Patching/MultiServerUpdateandPatching.py
+++ Server Updates and Patching/MultiServerUpdateandPatching.py
@@ -17,16 +17,16 @@ for index, row in df.iterrows():
     elif os_type.lower() == 'windows':
 # Use pywinrm for Windows servers
 
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect(server_ip, username, password) # test these for proper functionality
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(server_ip, username, password) # test these for proper functionality
 
-stdin, stdout, stderr = ssh.exec_command('sudo apt update && sudo apt upgrade -y')
-print(stdout.read().decode())
-ssh.close()
+    stdin, stdout, stderr = ssh.exec_command('sudo apt update && sudo apt upgrade -y')
+    print(stdout.read().decode())
+    ssh.close()
 
-session = winrm.Session('windows_server_ip', auth=('your_username', 'your_password'))
-encoded_ps = f'[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12; Invoke-Expression (New-Object System.Net.WebClient).DownloadString(\'https://chocolatey.org/install.ps1\'); choco upgrade all -y'
+    session = winrm.Session('windows_server_ip', auth=('your_username', 'your_password'))
+    encoded_ps = f'[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12; Invoke-Expression (New-Object System.Net.WebClient).DownloadString(\'https://chocolatey.org/install.ps1\'); choco upgrade all -y'
 
-result = session.run_ps(encoded_ps)
-print(result.std_out.decode())
+    result = session.run_ps(encoded_ps)
+    print(result.std_out.decode())
