c5e382dde92fc239227fa3dda4211f359904c728
pierceizfierce <pierceizfierce@yahoo.com>
2024-01-21 22:25:12 -0600

    Cleanup code

--- Server Updates and Patching/MultiServerUpdateandPatching.py
+++ Server Updates and Patching/MultiServerUpdateandPatching.py
@@ -11,23 +11,25 @@ for index, row in df.iterrows():
     password = row['Password']
     os_type = row['OS']
 
-    # Perform actions based on the OS type (Linux or Windows)
+# Perform actions based on the OS type (Linux or Windows)
     if os_type.lower() == 'linux':
-            # Use Paramiko for Linux servers
-    # ...
+        # Use pywinrm for Windows servers
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect('linux_server_ip', username='your_username',
+                    password='your_password')  # remove username and password
+
+        stdin, stdout, stderr = ssh.exec_command('sudo apt update && sudo apt upgrade -y')
+        print(stdout.read().decode())
+        ssh.close()
+
+# Use Paramiko for Linux servers
     elif os_type.lower() == 'windows':
-    # Use pywinrm for Windows servers
 
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect('linux_server_ip', username='your_username', password='your_password') # remove username and password
-
-stdin, stdout, stderr = ssh.exec_command('sudo apt update && sudo apt upgrade -y')
-print(stdout.read().decode())
-ssh.close()
-
-session = winrm.Session('windows_server_ip', auth=('your_username', 'your_password'))
-encoded_ps = f'[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12; Invoke-Expression (New-Object System.Net.WebClient).DownloadString(\'https://chocolatey.org/install.ps1\'); choco upgrade all -y'
+        session = winrm.Session('windows_server_ip', auth=('your_username', 'your_password'))
+        encoded_ps = f'[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12; ' \ 
+                     f'Invoke-Expression (New-Object System.Net.WebClient).DownloadString(' \
+                     f'\'https://chocolatey.org/install.ps1\'); choco upgrade all -y '
 
 result = session.run_ps(encoded_ps)
 print(result.std_out.decode())
