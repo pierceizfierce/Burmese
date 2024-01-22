d25beff2f4e4d0eb565872305b472904f3e60a8d
pierceizfierce <pierceizfierce@yahoo.com>
2024-01-21 19:59:09 -0600

    Cleanup code

--- Lessons/2dLists.py
+++ Lessons/2dLists.py
@@ -1,17 +1,17 @@
-#2D Lists
+# 2D Lists
 
-#a 2D list is a list where each item in the list is another list
+# a 2D list is a list where each item in the list is another list
 
 matrix = [
-    [1,2,3],
-    [4,5,6],
-    [7,8,9]
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
 ]
-# matrix[0][1]=20 #this will modify the values. instead of 2 it will be 20.
-# print(matrix [0][1]) #this will return the 2
+matrix[0][1] = 20  # this will modify the values. instead of 2 it will be 20.
+print(matrix[0][1])  # this will return the 2
 
-#you can use nested loops to iterate over the matrix
+# you can use nested loops to iterate over the matrix
 for row in matrix:
     for item in row:
         print(item)
-        print (item)
+        print(item)
